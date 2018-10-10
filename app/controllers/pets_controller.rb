62fc220f44d0c08d9ae1109f738f00451ef0248f
Patricia Nykaza <pnykaza@calstatela.edu>
2018-10-10 17:25:11 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,24 +2,59 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    puts params
+    puts @pet.class
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+      @pet.save
+    else
+      params["owner"]["name"] = Owner.find_by(id: params["pet"]["owner_id"][0]).name
+      @pet.owner = Owner.find_by(id: params["pet"]["owner_id"][0])
+      @pet.save
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  post '/pets/:id' do
+    puts params
+    if !params[:pet].keys.include?("owner_id")
+      # params["owner"]["owner_id"] = []
+      @pet.owner = Owner.find_by(id: params["owner"]["owner_id"])
+      
+      params["owner"]["name"] = Owner.find_by(id: params["pet"]["owner"]["owner_id"]).name
+
+    end
+    puts params
+    owner_name = params["owner"]["name"]
+    has_owner_name = !owner_name.empty?
+
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+
+    if has_owner_name
+      @pet.owner = Owner.create(name: owner_name)
+      @pet.save
+    end
 
     redirect to "pets/#{@pet.id}"
   end
